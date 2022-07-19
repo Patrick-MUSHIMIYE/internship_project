507f48b258c8fee8f78f95bc7729e6e086edbe11
MUSHIMIYE Patrick <p.mushimiy@alustudent.com>
2022-07-19 13:11:06 +0200

    email stage

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -10,6 +10,8 @@ import {
 } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { EmpDto } from './employee.dto';
+import * as SendGrid from '@sendgrid/mail';
+
 @Controller('employee')
 export class EmployeeController {
   constructor(private employeeService: EmployeeService) {}
@@ -20,7 +22,27 @@ export class EmployeeController {
   }
   @Post()
   public async postEmployees(@Body() employee: EmpDto) {
-    return this.employeeService.postEmployees(employee);
+    // return this.employeeService.postEmployees(employee);
+    this.employeeService.postEmployees(employee);
+
+
+    const mailContent = {
+      to: 'ntwari.hugues@gmail.com',
+      from: "ntwari.hugues@gmail.com",
+      subject: 'PATCHU_SUB',
+      text:'hello world',
+      html: '<h1> Hello world!</h1>'
+    };
+    try {
+
+      SendGrid.setApiKey("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");
+      const transport = await SendGrid.send(mailContent);
+      console.log("email sent");
+      
+      return transport;
+    } catch (e) {
+      console.log(e.message);
+    }
   }
 
   @Get(':employee_id')
